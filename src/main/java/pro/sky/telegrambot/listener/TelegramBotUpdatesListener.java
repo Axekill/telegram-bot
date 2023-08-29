a576d0b0fa94190db245b9ec3c19ef5dc34fdaac
Axekill <andrei3.ru@ya.ru>
2023-08-29 13:15:46 +0300

    добавил обработку команды /start

--- src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
+++ src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
@@ -3,6 +3,8 @@ package pro.sky.telegrambot.listener;
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
 import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.request.SendMessage;
+import com.pengrad.telegrambot.response.SendResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +17,7 @@ import java.util.List;
 public class TelegramBotUpdatesListener implements UpdatesListener {
 
     private Logger logger = LoggerFactory.getLogger(TelegramBotUpdatesListener.class);
+    private static final String START = "/start";
 
     @Autowired
     private TelegramBot telegramBot;
@@ -28,9 +31,36 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
     public int process(List<Update> updates) {
         updates.forEach(update -> {
             logger.info("Processing update: {}", update);
-            // Process your updates here
+            String message = update.message().text();
+            Long chatId = update.message().chat().id();
+            switch (message) {
+                case START->{
+                    String userName= update.message().chat().username();
+                    startCommand(chatId,userName);
+                }
+                default->unknownCommand(chatId);
+            }
+
         });
         return UpdatesListener.CONFIRMED_UPDATES_ALL;
     }
+
+
+    private void startCommand(Long chatId, String userName) {
+        var text = "Добро пожаловать в бот, %s !";
+        var formattedText = String.format(text, userName);
+        sendMessage(chatId, formattedText);
+    }
+
+    private void unknownCommand(Long chatId) {
+        var text = "Не удалось распознать команду!";
+        sendMessage(chatId, text);
+    }
+
+    private void sendMessage(Long chatId, String text) {
+        SendMessage message = new SendMessage(chatId, text);
+        SendResponse response = telegramBot.execute(message);
+    }
+
 
 }
